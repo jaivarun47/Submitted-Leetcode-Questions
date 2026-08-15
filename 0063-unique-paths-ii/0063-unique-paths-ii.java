6d86a66ea5dbebee1e982b982a6c20dd24540c89
Jai Varun <78930725+jaivarun47@users.noreply.github.com>
2026-08-15 18:29:58 +0530

    Time: 1 ms (17.09%), Space: 43.3 MB (91.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,19 +1,18 @@
 class Solution {
-    public int possiblePath(int[][] obstacleGrid, int[][] dp, int x, int y){
+    public int path(int[][] obstacleGrid, int[][] dp, int x, int y){
         if (x < 0 || y < 0) return 0;
         if (obstacleGrid[x][y] == 1) return 0;
         if (x == 0 && y == 0) return 1;
         if (dp[x][y] != -1) return dp[x][y];
-        int left = possiblePath(obstacleGrid, dp, x-1, y);
-        int top = possiblePath(obstacleGrid, dp, x, y-1);
-        return dp[x][y] = left + top;
+        return dp[x][y] = path(obstacleGrid, dp, x-1, y) + path(obstacleGrid, dp, x, y-1);
+
     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid){
-        int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
         for (int[] num: dp){
             Arrays.fill(num, -1);
         }
-        return possiblePath(obstacleGrid, dp, obstacleGrid.length - 1, obstacleGrid[0].length - 1);
+        return path(obstacleGrid, dp, n-1, m-1);
     }
-    
 }
