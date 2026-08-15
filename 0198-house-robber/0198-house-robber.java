fb040cb901bf1501861a6bb16083bc8b01772a06
Jai Varun <78930725+jaivarun47@users.noreply.github.com>
2026-08-15 17:20:58 +0530

    Time: 0 ms (100%), Space: 43 MB (5.71%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,15 +1,17 @@
 class Solution {
-    public int robberHehe(int[] dp, int[] nums, int idx){
-        if (idx < 0) return 0;
+    public int robbery(int[] dp, int[] nums, int idx){
+        if (idx < 0){
+            return 0;
+        }
+
         if (dp[idx] != -1) return dp[idx];
-        int left =  robberHehe(dp, nums, idx-1);
-        int right = nums[idx] + robberHehe(dp, nums, idx-2);
-        return dp[idx] = Math.max(left, right);            
+
+        return dp[idx] = Math.max(robbery(dp, nums, idx - 1), nums[idx] + robbery(dp, nums, idx - 2));
     }
 
     public int rob(int[] nums){
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        return robberHehe(dp, nums, nums.length - 1);
+        return robbery(dp, nums, nums.length-1);
     }
 }
