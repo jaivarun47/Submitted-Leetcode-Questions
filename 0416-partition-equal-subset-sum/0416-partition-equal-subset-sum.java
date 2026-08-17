b6238eba3a1ae76f0040a76ac4c5977c34f1beb7
Jai Varun <78930725+jaivarun47@users.noreply.github.com>
2026-08-17 16:10:20 +0530

    Time: 34 ms (65.72%), Space: 47.8 MB (46.23%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,19 +1,26 @@
 class Solution {
-    public boolean subarray(int[] nums, Boolean[][] dp, int sum, int idx){
-        if (sum == 0) return true;
-        if (idx < 0) return false;
-        if (dp[idx][sum] != null) return dp[idx][sum];
-        boolean notTake = subarray(nums, dp, sum, idx-1);
-        boolean take = false;
-        if (nums[idx] <= sum){
-            take = subarray(nums, dp, sum - nums[idx], idx-1);
-        }
-        return dp[idx][sum] = take || notTake;
-    }
     public boolean canPartition(int[] nums){
-        int sum = 0;
-        for (int num:nums) sum += num;
-        Boolean[][] dp = new Boolean[nums.length][sum/2 + 1];
-        return (sum)%2 != 0 ? false : subarray(nums, dp, sum/2, nums.length-1);
-    }   
+        int arrSum = 0;
+        for (int num:nums) arrSum += num;
+        if (arrSum%2 != 0) return false;
+        int target = arrSum/2;
+        boolean[][] dp = new boolean[nums.length][target+1];
+        for (int i = 0; i < nums.length; i++){
+            dp[i][0] = true;
+        }
+        if (nums[0] <= target) dp[0][nums[0]] = true;
+
+        for (int i = 1; i < nums.length; i++){
+            for (int sum = 1; sum <= target; sum++){
+                boolean notTake = dp[i-1][sum];
+                boolean take = false;
+                if (nums[i] <= sum){
+                    take = dp[i-1][sum - nums[i]];
+                }
+                dp[i][sum] = take || notTake;
+            }
+            if (dp[i][target]) return true;
+        }
+        return false;
+    } 
 }
